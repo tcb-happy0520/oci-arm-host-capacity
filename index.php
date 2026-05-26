12ac58d07158a23e45d7735fec2805f78b687e41
happychen <HAPPY0520@tcb-bank.com.tw>
2026-05-26 15:32:24 +0800

    OK

--- index.php
+++ index.php
@@ -69,7 +69,6 @@ $shape = getenv('OCI_SHAPE');
 $maxRunningInstancesOfThatShape = 1;
 $maxInstancesEnv = getenv('OCI_MAX_INSTANCES');
 if ($maxInstancesEnv !== false && $maxInstancesEnv !== '') {
-    $maxRunningInstancesOfThatShape = (int) $maxInstancesEnv
     $maxRunningInstancesOfThatShape = (int) $maxInstancesEnv;
 }
 
